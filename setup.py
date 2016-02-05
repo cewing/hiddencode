9cff8103b4b614b31dfdaaf0ef6a9ecc7685ff00
cewing <cris@crisewing.com>
2016-02-05 10:18:07 -0800

    make package minimally installable by pip/setuptools

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='hiddencode',
+    version='1.0',
+    description='A Sphinx extension which provide a directive to render a hidden code block',
+    author='Anthony Scopatz',
+    author_email='',
+    license='MIT',
+    modules=['hidden_code_block'],
+    zip_safe=False
+)
